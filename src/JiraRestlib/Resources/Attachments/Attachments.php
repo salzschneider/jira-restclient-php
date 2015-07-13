8acd430e8824e3961447cfd9f47095e583265ded
Peter Sovago <sovagope@gmail.com>
2015-07-13 22:38:35 +0200

    Add all attachment resource functions

--- src/JiraRestlib/Resources/Attachments/Attachments.php
+++ src/JiraRestlib/Resources/Attachments/Attachments.php
@@ -8,7 +8,7 @@ class Attachments extends ResourcesAbstract
     /**
      * Add one or more attachments to an issue.
      * 
-     * @param string $idOrKey the issue id or key to update (i.e. JRA-1330)
+     * @param string $idOrKey the issue id or key (i.e. JRA-1330)
      * @param array $files filenames with full path - multipart posts
      * 
      * @return void
@@ -24,5 +24,73 @@ class Attachments extends ResourcesAbstract
         $this->setOptions(array("files" => $files));
         $this->uri = "/rest/api/".$this->getApiVersion()."/issue/".$idOrKey."/attachments";
         $this->method = "POST";
+    }
+    
+    /**
+     * Remove an attachment from an issue.
+     * 
+     * @param array $attachmentId id of the attachment that we will delete (10760)
+     * 
+     * @return void
+     * https://docs.atlassian.com/jira/REST/latest/#d2e3550
+     */
+    public function deleteAttachment($attachmentId)
+    {        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/attachment/".$attachmentId;        
+        $this->method = "DELETE";
+    }
+    
+    /**
+     * Returns the meta-data for an attachment, including the URI of the actual attached file.
+     * 
+     * @param array $attachmentId id of the attachment (10760)
+     * 
+     * @return void
+     * https://docs.atlassian.com/jira/REST/latest/#d2e3550
+     */
+    public function getAttachmentMetaById($attachmentId)
+    {        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/attachment/".$attachmentId;        
+        $this->method = "GET";
+    }
+      
+    /**
+     * Returns the meta information for an attachments, specifically if they are enabled and the maximum upload size allowed.
+     * 
+     * @return void
+     * https://docs.atlassian.com/jira/REST/latest/#d2e3550
+     */
+    public function getAttachmentMeta()
+    {        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/attachment/meta";        
+        $this->method = "GET";
+    }
+    
+    /**
+     * Tries to expand an attachment. Output is raw and should be backwards-compatible through the course of time.
+     * 
+     * @param array $attachmentId id of the attachment (10760)
+     * 
+     * @return void
+     * https://docs.atlassian.com/jira/REST/latest/#d2e3550
+     */
+    public function getAttachmentExpandRaw($attachmentId)
+    {        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/attachment/".$attachmentId."/expand/raw";        
+        $this->method = "GET";
+    }
+    
+    /**
+     * Tries to expand an attachment. Output is human-readable and subject to change.
+     * 
+     * @param array $attachmentId id of the attachment (10760)
+     * 
+     * @return void
+     * https://docs.atlassian.com/jira/REST/latest/#d2e3550
+     */
+    public function getAttachmentExpandHuman($attachmentId)
+    {        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/attachment/".$attachmentId."/expand/human";        
+        $this->method = "GET";
     }
 }
--- tests/integration/Resources/Attachments/AttachmentTest.php
+++ tests/integration/Resources/Attachments/AttachmentTest.php
@@ -2,6 +2,7 @@
 use Mockery as m;
 use JiraRestlib\Api\Api;
 use JiraRestlib\Config\Config;
+use JiraRestlib\Resources\Issue\Issue;
 use JiraRestlib\Resources\Attachments\Attachments;
 use JiraRestlib\Tests\IntegrationBaseTest;
 
@@ -57,6 +58,163 @@ class AttachmentTest extends IntegrationBaseTest
         $result = $api->getRequestResult($attachmentResource);
 
         $this->assertFalse($result->hasError()); 
+    }
+    
+    public function testDeleteAttachmentTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+        
+        $issueResource = new Issue();
+        $issueResource->getIssue(self::$foreverIssueId);
+        $result = $api->getRequestResult($issueResource);
+        $response = $result->getResponse();
+        $attachmentId = $response['fields']['attachment'][0]['id']; 
+        $attachmentCount = count($response['fields']['attachment']);
+
+        $attachmentResource->deleteAttachment($attachmentId);
+        $result = $api->getRequestResult($attachmentResource);     
+        $this->assertFalse($result->hasError()); 
+     
+        $result = $api->getRequestResult($issueResource);
+        $response = $result->getResponse();
+        $attachmentCountNew = count($response['fields']['attachment']);
+
+        $this->assertSame($attachmentCountNew, $attachmentCount-1);
+    }
+    
+    public function testDeleteAttachmentFalse()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+
+        $attachmentResource->deleteAttachment(0);
+        $result = $api->getRequestResult($attachmentResource);     
+        $this->assertTrue($result->hasError()); 
+    }
+    
+    public function testGetAttachmentMetaByIdTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+        
+        $issueResource = new Issue();
+        $issueResource->getIssue(self::$foreverIssueId);
+        $result = $api->getRequestResult($issueResource);
+        $response = $result->getResponse();
+        $attachmentId = $response['fields']['attachment'][0]['id']; 
+
+        $attachmentResource->getAttachmentMetaById($attachmentId);
+        $result = $api->getRequestResult($attachmentResource);        
+        $this->assertFalse($result->hasError()); 
+    }
+    
+    public function testGetAttachmentMetaByIdFalse()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+        $attachmentResource->getAttachmentMetaById(0);
+        $result = $api->getRequestResult($attachmentResource);        
+        $this->assertTrue($result->hasError()); 
+    }
+    
+    public function testGetAttachmentMetaTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+
+        $attachmentResource->getAttachmentMeta();
+        $result = $api->getRequestResult($attachmentResource);    
+        
+        $this->assertFalse($result->hasError()); 
+    }
+    
+    public function testGetAttachmentExpandRawTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+        
+        $issueResource = new Issue();
+        $issueResource->getIssue(self::$foreverIssueId);
+        $result = $api->getRequestResult($issueResource);
+        $response = $result->getResponse();
+        $attachmentId = $response['fields']['attachment'][0]['id']; 
+
+        $attachmentResource->getAttachmentExpandRaw($attachmentId);
+        $result = $api->getRequestResult($attachmentResource);  
+        $this->assertFalse($result->hasError()); 
+    }
+    
+    public function testGetAttachmentExpandRawFalse()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+
+        $attachmentResource->getAttachmentExpandRaw(0);
+        $result = $api->getRequestResult($attachmentResource);  
+        $this->assertTrue($result->hasError()); 
+    }
+    
+    public function testGetAttachmentExpandHumanTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+        
+        $issueResource = new Issue();
+        $issueResource->getIssue(self::$foreverIssueId);
+        $result = $api->getRequestResult($issueResource);
+        $response = $result->getResponse();
+        $attachmentId = $response['fields']['attachment'][0]['id']; 
+
+        $attachmentResource->getAttachmentExpandHuman($attachmentId);
+        $result = $api->getRequestResult($attachmentResource);        
+        $this->assertFalse($result->hasError()); 
+    }
+    
+    public function testGetAttachmentExpandHumanFalse()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+
+        $api = new Api($config);
+        $attachmentResource = new Attachments();
+
+        $attachmentResource->getAttachmentExpandHuman(0);
+        $result = $api->getRequestResult($attachmentResource);  
+        $this->assertTrue($result->hasError()); 
     }
 
 }
