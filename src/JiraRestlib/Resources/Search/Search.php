de9958263840988d162d620a5452f9b6835eb0a7
Peter Sovago <sovagope@gmail.com>
2015-08-03 22:51:06 +0200

    Add search resource

---
+++ src/JiraRestlib/Resources/Search/Search.php
@@ -0,0 +1,65 @@
+<?php
+namespace JiraRestlib\Resources\Search;
+use JiraRestlib\Resources\ResourcesAbstract;
+
+class Search extends ResourcesAbstract
+{
+    /**
+     * Searches for issues using JQL.
+     * If the JQL query is too large to be encoded as a query param you should instead POST to this resource.
+     * 
+     * @param string $jql Sorting the jql parameter is a full JQL expression, and includes an ORDER BY clause.
+     * @param integer $startAt The index of the first issue to return (0-based)
+     * @param integer $maxResults The maximum number of issues to return (defaults to 50). *     
+     * @param boolean $validateQuery Whether to validate the JQL query
+     * @param array $fields The list of fields to return for each issue. By default, all navigable fields are returned.
+     * @param array $expand A list of the parameters to expand.
+     * 
+     * @return void
+     */
+    public function doSearchGet($jql, $startAt = 0, $maxResults = 50, $validateQuery = true, $fields = array(), $expand = array())
+    {        
+        $parameters = array();
+
+        $parameters["jql"] = array($jql);
+        $parameters["startAt"] = array($startAt);
+        $parameters["maxResults"] = array($maxResults);
+        $parameters["validateQuery"] = $validateQuery ? array("true") : array("false");        
+        $parameters["fields"] = $fields;
+        $parameters["expand"] = $expand;
+                
+        $expandQuery = $this->getQueryUri($parameters);
+        
+        $this->uri = "/rest/api/".$this->getApiVersion()."/search/" . $expandQuery;        
+        $this->method = "GET";
+    }
+    
+    /**
+     * Performs a search using JQL. 
+     * If the JQL query is too large to be encoded as a query param you should instead POST to this resource.
+     * 
+     * @param string $jql Sorting the jql parameter is a full JQL expression, and includes an ORDER BY clause.
+     * @param integer $startAt The index of the first issue to return (0-based)
+     * @param integer $maxResults The maximum number of issues to return (defaults to 50). *     
+     * @param boolean $validateQuery Whether to validate the JQL query
+     * @param array $fields The list of fields to return for each issue. By default, all navigable fields are returned.
+     * @param array $expand A list of the parameters to expand.
+     * 
+     * @return void
+     */
+    public function doSearchPost($jql, $startAt = 0, $maxResults = 50, $validateQuery = true, $fields = array(), $expand = array())
+    {        
+        $parameters = array();
+
+        $parameters["jql"] = $jql;
+        $parameters["startAt"] = $startAt;
+        $parameters["maxResults"] = $maxResults;
+        $parameters["validateQuery"] = $validateQuery ? "true" : "false";        
+        $parameters["fields"] = $fields;
+        $parameters["expand"] = $expand;
+        
+        $this->setOptions(array("json" => $parameters));
+        $this->uri = "/rest/api/".$this->getApiVersion()."/search/";        
+        $this->method = "POST";
+    }
+}
---
+++ tests/integration/Resources/Search/SearchPostTest.php
@@ -0,0 +1,191 @@
+<?php
+use Mockery as m;
+use JiraRestlib\Api\Api;
+use JiraRestlib\Config\Config;
+use JiraRestlib\Resources\Search\Search;
+use JiraRestlib\Tests\IntegrationBaseTest;
+
+class SearchPostTest extends IntegrationBaseTest
+{
+
+    public function tearDown()
+    {
+        m::close();
+    }   
+   
+    public function testDoSearchGetDefaultTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "";        
+        $searchResource->doSearchPost($jql);
+        $result = $api->getRequestResult($searchResource);     
+        $this->assertFalse($result->hasError());  
+        
+        $response = $result->getResponse();
+        $issues = $response['issues'];        
+        $this->assertGreaterThanOrEqual(1, count($issues));
+        $secondIssueId = $issues[1]["id"];
+        
+        return $secondIssueId;
+    }  
+    
+    public function testDoSearchGetDefaultFalse()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "WRONG";        
+        $searchResource->doSearchPost($jql);
+        $result = $api->getRequestResult($searchResource);     
+        $this->assertTrue($result->hasError()); 
+    }  
+    
+    public function testDoSearchGetTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "issueKey = ".self::$foreverIssueId;
+        
+        $searchResource->doSearchPost($jql);
+        $result = $api->getRequestResult($searchResource);  
+        $this->assertFalse($result->hasError());     
+        
+        $response = $result->getResponse();
+        $issues = $response['issues'];        
+        $this->assertSame(count($issues), 1);        
+    }
+    
+    /**
+     * @depends testDoSearchGetDefaultTrue
+     */
+    public function testDoSearchGetStartAtTrue($secondIssueId)
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "";   
+        $startAt = 1;
+       
+        $searchResource->doSearchPost($jql, $startAt);
+        $result = $api->getRequestResult($searchResource);  
+        $this->assertFalse($result->hasError());     
+        
+        $response = $result->getResponse();
+        $issues = $response['issues'];        
+        $this->assertSame($issues[0]['id'], $secondIssueId);        
+    }
+
+    public function testDoSearchGetMaxResultTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "";   
+        $startAt = 0;
+        $maxResult = 2;
+       
+        $searchResource->doSearchPost($jql, $startAt, $maxResult);
+        $result = $api->getRequestResult($searchResource);  
+        $this->assertFalse($result->hasError());     
+        
+        $response = $result->getResponse();
+        $issues = $response['issues'];   
+   
+        $this->assertSame(count($issues), $maxResult);        
+    }
+    
+    public function testDoSearchGetValidateTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "issueKey = sdsd";
+        
+        $startAt = 0;
+        $maxResult = 50;
+
+        $searchResource->doSearchPost($jql, $startAt, $maxResult, true);
+        $result = $api->getRequestResult($searchResource);       
+        $this->assertTrue($result->hasError());        
+        
+        $searchResource->doSearchPost($jql, $startAt, $maxResult, false);
+        $result = $api->getRequestResult($searchResource);       
+        $this->assertFalse($result->hasError());   
+    }
+    
+    public function testDoSearchGetFieldsTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "";
+        $startAt = 0;
+        $maxResult = 50;
+
+        $searchResource->doSearchPost($jql, $startAt, $maxResult, true, array("summary", "comment"));
+        $result = $api->getRequestResult($searchResource);         
+        $this->assertFalse($result->hasError());        
+        
+        $response = $result->getResponse();
+        $fields = array_keys($response["issues"][0]["fields"]);
+        $diff = array_diff($fields, array("summary", "comment"));
+        $diff2 = array_diff(array("summary", "comment"), $fields);
+      
+        $this->assertTrue(empty($diff));
+        $this->assertTrue(empty($diff2));
+        
+    }
+    
+    public function testDoSearchGetExpandTrue()
+    {
+        $config = new Config(self::$jiraRestHost);
+        $config->setSSLVerification(self::$isVerified);
+        $config->setJiraAuth(self::$jiraRestUsername, self::$jiraRestPassword);
+        
+        $api = new Api($config);
+        $searchResource = new Search();
+
+        $jql = "";
+        $startAt = 0;
+        $maxResult = 2;
+
+        $searchResource->doSearchPost($jql, $startAt, $maxResult, true, array(), array("names", "schema"));
+        $result = $api->getRequestResult($searchResource);   
+        $this->assertFalse($result->hasError());    
+        
+        $response = $result->getResponse();
+        $this->assertArrayHasKey("names", $response); 
+        $this->assertArrayHasKey("schema", $response); 
+    }
+}
